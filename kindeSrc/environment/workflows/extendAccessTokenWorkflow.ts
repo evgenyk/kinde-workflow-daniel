9ca481b39140a702bd4463e49284d4fe5c870b38
Daniel Rivers <me@danielrivers.com>
2024-11-11 10:24:35 +0000

    Update extendAccessTokenWorkflow.ts

--- kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
+++ kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
@@ -3,7 +3,9 @@ export const workflowSettings = {
   id: "addAccessTokenClaim",
   trigger: "user:tokens_generation",
   name: "Add Access Token Claim",
-  
+  bindings: {
+    
+  }
 };
   
 export default {
