c370ae9e6852b234c18865d71b437bd05d887ae0
Daniel Rivers <me@danielrivers.com>
2024-11-11 10:28:11 +0000

    Update extendAccessTokenWorkflow.ts
    Update to complex

--- kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
+++ kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
@@ -1,15 +1,17 @@
+import { onUserTokenGeneratedEvent,  accessTokenCustomClaims, WorkflowSettings, WorkflowTrigger, denyAccess, fetch } from "@kinde/infrastructure"
 
-export const workflowSettings = {
+export const workflowSettings: WorkflowSettings = {
   id: "addAccessTokenClaim",
+  trigger: WorkflowTrigger.UserTokenGeneration,
   trigger: "user:tokens_generation",
   name: "Add Access Token Claim",
-  bindings: {
-    
-  }
+  bindings: {}
 };
-  
+
 export default {
-  async handle(event) {
-    kinde.accessToken.setCustomClaim('hello', "Hello there!");
+  async handle(event: onUserTokenGeneratedEvent) {
+    const accessToken = accessTokenCustomClaims<{ hello: string; ipAddress: string; ipAdd: string}>();
+    accessToken.hello = "Hello there!";
+    accessToken.ipAddress = event.request.ip
   }
 }
