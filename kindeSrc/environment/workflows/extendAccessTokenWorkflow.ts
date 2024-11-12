b8062608701343a7b3bfce38e1a1e3cf8b1c7cd5
Daniel Rivers <me@danielrivers.com>
2024-11-12 00:18:43 +0000

    Update extendAccessTokenWorkflow.ts

--- kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
+++ kindeSrc/environment/workflows/extendAccessTokenWorkflow.ts
@@ -1,4 +1,4 @@
-import { onUserTokenGeneratedEvent,version, getEnvironmentVariable, accessTokenCustomClaims, WorkflowSettings, WorkflowTrigger, denyAccess, fetch } from "@kinde/infrastructure"
+import { onUserTokenGeneratedEvent,version, createKindeAPI, getEnvironmentVariable, accessTokenCustomClaims, WorkflowSettings, WorkflowTrigger, denyAccess, fetch } from "@kinde/infrastructure"
 import { settings } from "../../../utils/utils.ts";
 // import { User } from "@kinde/managements-api-js/workflow"
 
@@ -15,72 +15,72 @@ export const workflowSettings: WorkflowSettings = {
   }
 };
 
-export async function createKindeAPI(baseURL: string) {
-  const M2M_CLIENT_ID = getEnvironmentVariable("WF_M2M_CLIENT_ID");
-  const M2M_CLIENT_SECRET = getEnvironmentVariable("WF_M2M_CLIENT_SECRET");
+// export async function createKindeAPI(baseURL: string) {
+//   const M2M_CLIENT_ID = getEnvironmentVariable("WF_M2M_CLIENT_ID");
+//   const M2M_CLIENT_SECRET = getEnvironmentVariable("WF_M2M_CLIENT_SECRET");
 
-  const errors = [];
+//   const errors = [];
 
-  if (!M2M_CLIENT_ID) {
-    errors.push("WF_M2M_CLIENT_ID not set");
-  }
+//   if (!M2M_CLIENT_ID) {
+//     errors.push("WF_M2M_CLIENT_ID not set");
+//   }
 
-  if (!M2M_CLIENT_SECRET) {
-    errors.push("WF_M2M_CLIENT_SECRET not set");
-  }
+//   if (!M2M_CLIENT_SECRET) {
+//     errors.push("WF_M2M_CLIENT_SECRET not set");
+//   }
 
-  if (!URLSearchParams) {
-    errors.push("url binding not available");
-  }
+//   if (!URLSearchParams) {
+//     errors.push("url binding not available");
+//   }
 
-  console.log('errors', errors);
+//   console.log('errors', errors);
 
-  console.log('M2MID', M2M_CLIENT_ID?.value, 'M2MSecret', M2M_CLIENT_SECRET?.value);
+//   console.log('M2MID', M2M_CLIENT_ID?.value, 'M2MSecret', M2M_CLIENT_SECRET?.value);
 
 
-  const token = await fetch(`${baseURL}/oauth2/token`, {
-    method: "POST",
-    responseFormat: "json",
-    headers:{
-      "Content-type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      audience: `${baseURL}/api`,
-      grant_type: "client_credentials",
-      client_id: M2M_CLIENT_ID?.value,
-      client_secret: M2M_CLIENT_SECRET?.value,
-    }),
-  });
+//   const token = await fetch(`${baseURL}/oauth2/token`, {
+//     method: "POST",
+//     responseFormat: "json",
+//     headers:{
+//       "Content-type": "application/x-www-form-urlencoded",
+//     },
+//     body: new URLSearchParams({
+//       audience: `${baseURL}/api`,
+//       grant_type: "client_credentials",
+//       client_id: M2M_CLIENT_ID?.value,
+//       client_secret: M2M_CLIENT_SECRET?.value,
+//     }),
+//   });
 
-  console.log(token);
+//   console.log(token);
   
-  const callKindeAPI = async (
-    method: "POST" | "GET" | "PUT" | "DELETE" | "PATCH",
-    endpoint: string,
-    params: Record<string, string>,
-  ) =>
-    await kinde.fetch(`${baseURL}/api/v1/${endpoint}`, {
-      method,
-      responseFormat: "json",
-      headers: {
-        authorization: `Bearer ${token.json.access_token}`,
-        "Content-Type": "application/json",
-        "accept": "application/json"
-      },
-      body: new URLSearchParams(params),
-    });
+//   const callKindeAPI = async (
+//     method: "POST" | "GET" | "PUT" | "DELETE" | "PATCH",
+//     endpoint: string,
+//     params: Record<string, string>,
+//   ) =>
+//     await kinde.fetch(`${baseURL}/api/v1/${endpoint}`, {
+//       method,
+//       responseFormat: "json",
+//       headers: {
+//         authorization: `Bearer ${token.json.access_token}`,
+//         "Content-Type": "application/json",
+//         "accept": "application/json"
+//       },
+//       body: new URLSearchParams(params),
+//     });
     
-  return {
-    get: async (endpoint: string, params: Record<string, string>) =>
-      await callKindeAPI("GET", endpoint, params),
-    post: async (endpoint: string, params: Record<string, string>) =>
-      await callKindeAPI("PATCH", endpoint, params),
-    put: async (endpoint: string, params: Record<string, string>) =>
-      await callKindeAPI("PUT", endpoint, params),
-    delete: async (endpoint: string, params: Record<string, string>) =>
-      await callKindeAPI("DELETE", endpoint, params),
-  };
-}
+//   return {
+//     get: async (endpoint: string, params: Record<string, string>) =>
+//       await callKindeAPI("GET", endpoint, params),
+//     post: async (endpoint: string, params: Record<string, string>) =>
+//       await callKindeAPI("PATCH", endpoint, params),
+//     put: async (endpoint: string, params: Record<string, string>) =>
+//       await callKindeAPI("PUT", endpoint, params),
+//     delete: async (endpoint: string, params: Record<string, string>) =>
+//       await callKindeAPI("DELETE", endpoint, params),
+//   };
+// }
 
 
 export default {
