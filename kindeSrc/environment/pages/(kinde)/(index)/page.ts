309cc6fc994ac06193df60b0313defa46c782426
Evgeny <860694+evgenyk@users.noreply.github.com>
2024-11-19 16:47:01 +1100

    Update page.ts

--- kindeSrc/environment/pages/(kinde)/(index)/page.ts
+++ kindeSrc/environment/pages/(kinde)/(index)/page.ts
@@ -3,5 +3,5 @@
 export const pageSettings = {};
 
 export default async function handleRequest(event: any) {
-    return '<html><body><h1>Page updated</h1></body></html>';
+    return '<html><body><h1>Page updated something</h1></body></html>';
 }
